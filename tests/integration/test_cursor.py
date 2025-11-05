cc593d60223ce876a76a58d3d337c3d1de561c03
James Robinson <jrobinson@confluent.io>
2025-11-05 11:45:56 -0500

    Remove LIMIT clause.

--- tests/integration/test_cursor.py
+++ tests/integration/test_cursor.py
@@ -1,13 +1,13 @@
 from confluent_sql.statement import Phase
 
-"""A one column, one row very fast to complete query."""
-SINGLE_ROW_QUERY = "SELECT 42 as answer FROM `INFORMATION_SCHEMA`.`TABLES` LIMIT 1"
+"""A one column very fast to complete query."""
+SINGLE_COLUMN_QUERY = "SELECT 42 as answer FROM `INFORMATION_SCHEMA`.`TABLES`"
 # (Queries against INFORMATION_SCHEMA execute very quickly)
 
 
 def test_cursor_metadata(cursor):
     # 'Cursor.execute' defaults to snapshot queries
-    cursor.execute(SINGLE_ROW_QUERY)
+    cursor.execute(SINGLE_COLUMN_QUERY)
 
     assert cursor._statement.is_bounded is True
     assert cursor._statement.phase is Phase.COMPLETED
@@ -24,7 +24,7 @@ def test_unbounded_query_with_finite_statement(cursor):
     # Even if we set `bounded` to False here, we still get
     # a bounded statement since it ends and no sources are
     # generating more input
-    cursor.execute(SINGLE_ROW_QUERY, bounded=False)
+    cursor.execute(SINGLE_COLUMN_QUERY, bounded=False)
     assert cursor._statement.is_bounded is True
 
 
